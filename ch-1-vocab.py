111add973978747219c96e2b115b0955e72507e0
Justin Clark <justinclark.dev@gmail.com>
2020-01-08 08:33:23 -0800

    adding first files for class

---
+++ ch-1-vocab.py
@@ -0,0 +1,52 @@
+## Practice Activity -- Chapter 1 Vocabulary
+    Due: Friday, Jan 10 in class
+## NOTE: This activity will be performed IN LAB on the scheduled day; a worksheet will be handed out at the beginning of lab.
+
+## Write a short answer for each of the following:
+
+# What is a software? List at least 3 categories/kinds of software.
+    Software is a program, or an organized set of instructions for a computer that solves a task.
+        web applications
+        databases
+        artificial intelligence 
+
+# What is a hardware? List at least 3 categories/kinds of hardware.
+    Hardware is physical equipment that runs or supports the running of software.
+
+# What is the difference between a low-level programming language and a high-level programming language? What kind of language is Python?
+    A low-level programming language is one which operates directly on the hardware.
+        e.g., C++, COBAL, Fortran
+    A high-level programming language is one which needs to be compiled to run on a low-level language.
+        e.g., C#, JavaScript, Java
+# What is a key word? Define the term in your own words and give at least one example.
+    A keyword is word that is used by a programming language for things like functions and syntax
+
+# What is an operator? Define the term in your own words and give at least one example.
+    An operator is highly trained soldier in an elite military service.
+    But in programming, an operator is special character that performs mathematical operations.
+        e.g., +, -, *, /, %, <, >, etc.
+
+# Define the word syntax.
+    Syntax means the exact way and exact keywords to use in order for a program to be executed by a computer. Although syntax differs from language to language, many languages share many aspects of syntax.
+
+# What does the word statement mean in computer programming?
+    A statement is an instruction to the computer.
+
+# What is a bit? What is a byte?
+    A bit is a one or a zero that is used to open or close circuts on a computer chip.
+    A byte is a series of 8 bits.
+
+# Convert the following numbers from base 2 to base 10. Show your work:
+
+10110>sub_2	100011>sub_2
+# Convert the following numbers from base 10 to base 2. Show your work:
+
+44>sub_10	21>sub_10
+# State in your own words the class policy on copying code from another student? (You can find this information on the course syllabus; be sure to read the whole syllabus carefully.)
+    Not recommened for the purpose of learning, and if turned in as if it were there own... not allowed!
+# What section number are you in?  (Be sure to always show this number in the comment block you write at the beginning of each program.)
+    Section 03
+# What is the scheduled date and time for the final exam for your section?  (You can find this information at the bottom of the General Course Information section on the class home page; plan to take the final exam at the scheduled time.)
+    March 23, 2020 @1:30pm
+
+## NOTE: Most lab deadlines are set at 11pm on the day of the lab meeting.  When electronic submission is required, this is the actual deadline.  When the submission is on paper (as it is for this lab), the worksheet must be turned in no later than the very beginning of the next class meeting.  So, for this lab, be sure to turn in your completed worksheet at the end of lab, or at the very latest at the beginning or our next class meeting.  A complete solution will be posted after that.
