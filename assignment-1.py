c4fe4e2ea63db8e807b933a7a56e90ede93c2c25
Justin Clark <44019389+justinclark-dev@users.noreply.github.com>
2020-01-10 11:00:30 -0800

    adding assignment 1

---
+++ assignment-1.py
@@ -0,0 +1,64 @@
+# CSC 110 - Assignment #1
+# Input and Output of Strings
+# Section 03
+# Justin Clark
+# 1/8/2020
+
+# nouns
+# verbs
+# adjectives
+# adverbs
+
+# Program requirements:
+# =========================================
+# at least 3 sentences
+# at least 6 words
+# 
+# When your program starts, have it print a message to welcome the user to your game.
+# 
+# Next, the program prompts the user for each word, one at a time. Make sure the program 
+#   tells the user what type of word to enter with each prompt.
+# 
+# Once the user has entered the last word, the program should print the story with the 
+#   user's words inserted into the story in the correct places.
+#       Important NOTE: Your output will NOT show any words underlined or bold or in italics 
+#           as you see above.  I have added that formatting here just to make it clear which words 
+#           are being replaced in the output with the user's choices.
+
+numberToConvert = int(input('Please enter a number you wish to convert to Base 10: '))
+
+nounPlace = input('Enter a place: ')
+nounPerson = input('Enter a person: ')
+nounThing = input('Enter a thing: ')
+
+verbs
+# a word used to describe an action, state, or occurrence, and forming the main part of the 
+#   predicate of a sentence, such as hear, become, happen
+verbAction = input('Enter a : ')
+verbState = input('Enter a : ')
+verbOccurrence = input('Enter a : ')
+
+adjectives
+# a word or phrase naming an attribute, added to or grammatically related to a noun to modify or describe it.
+verb = input('Enter a : ')
+verb = input('Enter a : ')
+verb = input('Enter a : ')
+
+adverbs
+# a word or phrase that modifies or qualifies an adjective, verb, or other adverb or a word group, expressing a relation of place, time, circumstance, manner, cause, degree, etc. (e.g., gently, quite, then, there ).
+verb = input('Enter a : ')
+verb = input('Enter a : ')
+verb = input('Enter a : ')
+
+
+madlib = ""
+
+
+
+# Written Report
+# =========================================
+# Include a comment block at the end of your program with a short written report addressing the following questions:
+
+#     How did you go about starting this assignment?  How did you come up with your story?
+#     How did you test your program?  What works and what doesn't?
+#     What did you learn from this assignment?  What would you do differently next time?
